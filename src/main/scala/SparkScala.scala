6612504d9ed5a045f15db09e0cbcafe243cbcaf3
atheodosiou <anastasios.theodosiou@gmail.com>
2018-10-23 00:44:54 +0300

    changes

--- src/main/scala/SparkScala.scala
+++ src/main/scala/SparkScala.scala
@@ -8,12 +8,27 @@ object SparkScala {
     //Use to set the loggig levet to ERROR
     Logger.getLogger("org").setLevel(Level.ERROR)
 
-    val sc = new SparkContext("local[*]","Spark Tutorial")
+    val sc = new SparkContext("local[*]","RatingsCounter")
+
+    println("Reading data...")
 
     //Load up each line of the ratings data into an RDD
     val lines = sc.textFile("src/Data/ml-100k/u.data")
 
     val ratings = lines.map(x=>x.toString().split("\t")(2))
+
+//    val movies =lines.map(x=>x.toString().split("\t")(1))
+//    val users = lines.map(x=>x.toString().split("\t")(0))
+//
+//    val totalUsers=users.distinct().count()
+//    users.distinct().foreach(println)
+//    val usrRatings=users.countByValue();
+//    val usrResults=usrRatings.toSeq.sortBy(_._1)
+//    usrResults.foreach(println)
+//    val totalMovies=movies.distinct().count()
+//
+//    println("Number of total users: "+totalUsers)
+//    println("Number of total movies: "+totalMovies)
 
     val results = ratings.countByValue()
 
